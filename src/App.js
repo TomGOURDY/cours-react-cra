ce52d102d40dbb3a7d8b290b4dc599653497485d
TomGOURDY <tom.gourdy@edu.devinci.fr>
2022-02-02 16:39:00 +0100

    fixing warnings

--- src/App.js
+++ src/App.js
@@ -1,75 +1,75 @@
-import Card from "./components/Card"
-import Plage from "./img/carnon-plage.jpg"
-import Roadmap from "./components/RoadMap"
+// import Card from "./components/Card"
+// import Plage from "./img/carnon-plage.jpg"
+// import Roadmap from "./components/RoadMap"
 import MyRouter from "./routes/MyRouter"
 
 function App() {
-  const user = {
-    firstname: "Machin", 
-    lastname: "Truc"
-  }
+  // const user = {
+  //   firstname: "Machin", 
+  //   lastname: "Truc"
+  // }
 
-  const fullName = user.firstname + " " + user.lastname
+  // const fullName = user.firstname + " " + user.lastname
 
-  function RoadMapList() {
-    const DataRoadMap = [{
-      percentage: '0%',
-      title: 'Titre 1',
-      content: 'Content 1',
-    }, {
-      percentage: '10%',
-      title: 'Titre 10°',
-      content: 'Content 10°',
-    }, {
-      percentage: '20%',
-      title: 'Titre 1',
-      content: 'Content 1',
-    }, {
-      percentage: '30%',
-      title: 'Titre 1',
-      content: 'Content 1',
-    }, {
-      percentage: '40%',
-      title: 'Titre 1',
-      content: 'Content 1',
-    }, {
-      percentage: '50%',
-      title: 'Titre 1',
-      content: 'Content 1',
-    }, {
-      percentage: '60%',
-      title: 'Titre 1',
-      content: 'Content 1',
-    }, {
-      percentage: '70%',
-      title: 'Titre 1',
-      content: 'Content 1',
-    }, {
-      percentage: '80%',
-      title: 'Titre 1',
-      content: 'Content 1',
-    },{
-      percentage: '90%',
-      title: 'Titre 1',
-      content: 'Content 1',
-    },{
-      percentage: '100%',
-      title: 'Titre 1',
-      content: 'Content 1',
-    }];
+  // function RoadMapList() {
+  //   const DataRoadMap = [{
+  //     percentage: '0%',
+  //     title: 'Titre 1',
+  //     content: 'Content 1',
+  //   }, {
+  //     percentage: '10%',
+  //     title: 'Titre 10°',
+  //     content: 'Content 10°',
+  //   }, {
+  //     percentage: '20%',
+  //     title: 'Titre 1',
+  //     content: 'Content 1',
+  //   }, {
+  //     percentage: '30%',
+  //     title: 'Titre 1',
+  //     content: 'Content 1',
+  //   }, {
+  //     percentage: '40%',
+  //     title: 'Titre 1',
+  //     content: 'Content 1',
+  //   }, {
+  //     percentage: '50%',
+  //     title: 'Titre 1',
+  //     content: 'Content 1',
+  //   }, {
+  //     percentage: '60%',
+  //     title: 'Titre 1',
+  //     content: 'Content 1',
+  //   }, {
+  //     percentage: '70%',
+  //     title: 'Titre 1',
+  //     content: 'Content 1',
+  //   }, {
+  //     percentage: '80%',
+  //     title: 'Titre 1',
+  //     content: 'Content 1',
+  //   },{
+  //     percentage: '90%',
+  //     title: 'Titre 1',
+  //     content: 'Content 1',
+  //   },{
+  //     percentage: '100%',
+  //     title: 'Titre 1',
+  //     content: 'Content 1',
+  //   }];
 
-    const roadMapItems = DataRoadMap.map((item) => 
-      <div>
-        <Roadmap percentage={item.percentage} title={item.title} content={item.content} />
-      </div>
-    );
+  //   const roadMapItems = DataRoadMap.map((item) => 
+  //     <div>
+  //       <Roadmap percentage={item.percentage} title={item.title} content={item.content} />
+  //     </div>
+  //   );
 
-    return (
-      <div>
-        {roadMapItems}
-      </div>
-    )
-  }
+  //   return (
+  //     <div>
+  //       {roadMapItems}
+  //     </div>
+  //   )
+  // }
   return (
     <>
     <MyRouter/>
